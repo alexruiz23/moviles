28846e0ee5a5af44f02f93239eaa9805ed069d46
administrador <administrador@Xubuntu22.salesianas.org>
2024-09-26 10:17:59 +0200

    26 de septiembre

---
+++ Ejemplo_1/src/app/componentes/asignatura/asignatura.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asignatura',
+  standalone: true,
+  imports: [],
+  templateUrl: './asignatura.component.html',
+  styleUrl: './asignatura.component.css'
+})
+export class AsignaturaComponent {
+
+}
